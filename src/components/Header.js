351ce1beccccee2180c6aba0d0bc1f019b8b8039
Lisandro Cresta <lisandrocresta99@gmail.com>
2023-07-31 14:49:35 -0300

    v-0.0.6
    -Cambios logo
    -cambios scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
 
     return (
         <header id="header" className="header flex justify-between items-center">
-            <img src={imagen} alt="logo" width={100} className="py-3 px-2" />
+            <img src={imagen} alt="logo" className="logo py-3 px-2" />
             <h1>Bienvenido a ArgSkins!</h1>
             <NavBar />
         </header>
